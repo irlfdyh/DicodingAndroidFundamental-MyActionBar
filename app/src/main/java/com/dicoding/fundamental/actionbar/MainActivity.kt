c32e591623e7404d51d00fa5068928a789435720
Irshal Firdeansyah <irshalfir@gmail.com>
2023-03-31 10:56:07 +0700

    add option menu

--- app/src/main/java/com/dicoding/fundamental/actionbar/MainActivity.kt
+++ app/src/main/java/com/dicoding/fundamental/actionbar/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.dicoding.fundamental.actionbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.dicoding.fundamental.actionbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu1 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, MenuFragment())
+                    .addToBackStack(null)
+                    .commit()
+                true
+            }
+            R.id.menu2 -> {
+                Intent(this, MenuActivity::class.java).apply {
+                    startActivity(this)
+                }
+                true
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
 }
